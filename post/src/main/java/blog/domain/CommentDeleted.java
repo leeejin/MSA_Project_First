16bdcd29fce2f2ce1a23757746814add1cf36a60
leeejin <asd72621@naver.com>
2024-01-23 15:29:29 +0900

    first commit

---
+++ post/src/main/java/blog/domain/CommentDeleted.java
@@ -0,0 +1,17 @@
+package blog.domain;
+
+import blog.domain.*;
+import blog.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class CommentDeleted extends AbstractEvent {
+
+    private Long id;
+    private String content;
+    private Long postId;
+    private String nickname;
+   
+}
